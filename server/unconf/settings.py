15abd941fafee6826cd09057f572bcc57ccfcb0e
kyoung <mail@kyoung.ca>
2018-01-30 21:46:10 -0800

    continuing heroku stuff

--- server/unconf/settings.py
+++ server/unconf/settings.py
@@ -131,6 +131,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
